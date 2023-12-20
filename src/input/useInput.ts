867e195b2b6b6913a83030cf0f65b01c8553d4a8
Sejin Cha <chasj0326@naver.com>
2023-12-20 14:22:00 +0900

    feat: input 관련 훅 구현

---
+++ src/input/useInput.ts
@@ -0,0 +1,22 @@
+import React, { useState } from 'react';
+
+const useInput = (initial?: string) => {
+  const [value, setValue] = useState(initial);
+
+  const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setValue(e.target.value);
+  };
+
+  const getInputProps = () => ({
+    value,
+    onChange,
+  });
+
+  return {
+    value,
+    onChange,
+    getInputProps,
+  };
+};
+
+export default useInput;
