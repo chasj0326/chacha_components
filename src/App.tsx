a0138e99f3189a6f755bb743f78651335e4e0be9
Sejin Cha <chasj0326@naver.com>
2023-12-26 17:50:59 +0900

    feat: App 코드 초기화

--- src/App.tsx
+++ src/App.tsx
@@ -1,54 +1,5 @@
-import { useTab, Tab } from './features/Tab';
-
 const App = () => {
-  const { getTabItemProps, getTabScreen } = useTab();
-
-  return (
-    <>
-      <div>
-        <h2>Tab1</h2>
-        <div style={{ display: 'flex', gap: '10px' }}>
-          <div {...getTabItemProps(0)}>Tab0</div>
-          <div {...getTabItemProps(1)}>Tab1</div>
-          <div {...getTabItemProps(2)}>Tab2</div>
-          <div {...getTabItemProps(3)}>Tab3</div>
-          <div {...getTabItemProps(4)}>Tab4</div>
-        </div>
-        <hr />
-        <div>
-          {getTabScreen([
-            <div>Tab Screen 0</div>,
-            <div>Tab Screen 1</div>,
-            <div>Tab Screen 2</div>,
-            <div>Tab Screen 3</div>,
-            <div>Tab Screen 4</div>,
-          ])}
-        </div>
-      </div>
-      <Tab.Box
-        onChange={(index) => console.log(index)}
-        initial={2}>
-        <h1>Tab Component</h1>
-        <Tab.Screens>
-          <Tab.Screen>thumbnail1</Tab.Screen>
-          <Tab.Screen>thumbnail2</Tab.Screen>
-          <Tab.Screen>thumbnail3</Tab.Screen>
-        </Tab.Screens>
-        <hr />
-        <Tab.List style={{ display: 'flex', gap: '10px' }}>
-          <Tab>tab1</Tab>
-          <Tab>tab2</Tab>
-          <Tab>tab3</Tab>
-        </Tab.List>
-        <hr />
-        <Tab.Screens>
-          <Tab.Screen>screen1</Tab.Screen>
-          <Tab.Screen>screen2</Tab.Screen>
-          <Tab.Screen>screen3</Tab.Screen>
-        </Tab.Screens>
-      </Tab.Box>
-    </>
-  );
+  return <></>;
 };
 
 export default App;
